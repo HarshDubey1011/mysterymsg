c0eb68b2e3d1136d1b1644f915ff3b1dacf954f9
HarshDubey1011 <pandetahallu@gmail.com>
2024-07-21 19:00:09 +0530

    implemented form

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,55 @@
-import React from 'react'
+'use client';
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useDebounceValue } from 'usehooks-ts'
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/router";
+import { signUpSchema } from "@/schemas/signUpSchema";
+import axios, {AxiosError} from 'axios';
+import { ApiResponse } from "@/types/ApiResponse";
 
-function page() {
+const page = () => {
+  const [username, setUsername] = useState('');
+  const [usernameMessage, setUsernameMessage] = useState('');
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const {toast} = useToast()
+  const router = useRouter();
+  const debouncedUsername = useDebounceValue(username, 300);
+  //zod implementation
+
+  const form = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      username: '',
+      email:'',
+      password: ''
+    }
+  })
+
+  useEffect(() => {
+    const checkUsernameUnique = async () => {
+      if(debouncedUsername) {
+        setIsCheckingUsername(true)
+        setUsernameMessage('')
+        try {
+          const response = await axios.get(`/api/check-username-unique?username=${debouncedUsername}`,)
+          setUsernameMessage(response.data.message);
+        } catch (error) {
+          const axiosError = error as AxiosError<ApiResponse>;
+          setUsernameMessage(axiosError.response?.data.message ?? "Error checking username")
+        }
+        finally {
+          setIsCheckingUsername(false)
+        }
+      }
+    }
+    checkUsernameUnique()
+  }, [debouncedUsername])
+
   return (
     <div>This is sign in page</div>
   )
